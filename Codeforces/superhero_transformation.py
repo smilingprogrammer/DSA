7d002aaebb544c2df566494a4efcfc8088ed053c
smilingprogrammer <abdulsoburoyewale@gmail.com>
2026-04-06 17:04:02 +0100

    Solved more leetcode solution

--- Codeforces/superhero_transformation.py
+++ Codeforces/superhero_transformation.py
@@ -6,19 +6,32 @@ consonant = "bcdfghjklmnpqrstvwxyz"
 
 ans = "No"
 
-i = j = 0
+# i = j = 0
 
-while i < len(s) and j < len(t):
-    if s[i] in vowel and t[j] in vowel:
-        ans = "Yes"
+# while i < len(s) and j < len(t):
+#     if s[i] in vowel and t[j] in vowel:
+#         ans = "Yes"
 
-    elif s[i] in consonant and t[j] in consonant:
-        ans = "Yes"
+#     elif s[i] in consonant and t[j] in consonant:
+#         ans = "Yes"
 
-    else:
-        ans = "No"
+#     else:
+#         ans = "No"
 
-    i += 1
-    j += 1
+#     i += 1
+#     j += 1
 
-print(ans)
+# print(ans)
+
+
+if len(s) != len(t):
+    print("No")
+else:
+    ans = "Yes"
+    for i in range(len(s)):
+        if (s[i] in vowels and t[i] in vowels) or (s[i] in consonants and t[i] in consonants):
+            continue
+        else:
+            ans = "No"
+            break
+    print(ans)
